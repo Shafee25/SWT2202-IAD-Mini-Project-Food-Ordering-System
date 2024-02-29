a492f3252c23404393150032b2d5f90d24e533d5
Shafee25 <shafeeahamed494@gmail.com>
2024-02-29 22:11:16 +0530

    IAD Project

---
+++ include/foot.php
@@ -0,0 +1,51 @@
+<footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                              </li>
+                          </ul>
+                      </div>
+
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;Address</h5>
+                          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;Oluvil, Ampara, Srilanka. <br><b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;Phone: +94751234567</b></a></p>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Ordering delicious food online has never been easier! Browse our extensive menu, select your favorite dishes, and customize your order to suit your taste. With just a few clicks, your culinary cravings will be on their way to your doorstep</p>
+                      </div>
+
+                </div>
+                <div class="row">
+                    <hr>
+                    <br>
+                    <div class="col-4">
+
+                    </div>
+                    <div class="col-4">
+                        <center><p style="color: white; size: 16px;"> &copy; Food Zone (Pvt) Ltd. All Rights Reserved</p></center>
+                    </div>
+                </div>
+            </div>
+
+     </div>
+</footer>
---
+++ portfolio.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");  
+error_reporting(0);  
+session_start(); 
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title> FooD ZonE | Home </title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="home">
+    <header id="header" class="header-scroll top-header headrom">
+        <nav class="navbar navbar-dark">
+            <div class="container">
+                <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/logo.png" alt="" width="18%"> </a>
+                <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                        <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                        <li class="nav-item"> <a class="nav-link active" href="portfolio.php">Portfolio <span class="sr-only"></span></a> </li>
+                        <?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+
+    <section class="hero bg-image" data-image-src="images/img/pimg.jpg">
+        <div class="hero-inner">
+            <div class="container text-center hero-text font-white">
+                <h1>Food Zone <br> Members  Gallery </h1>
+            </div>
+        </div>
+    </section>
+
+    <section class="popular">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <!-- <div class="title text-xs-center m-b-30"> -->
+                        <div class="card" style="width: 18rem;">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active img-thumbnail">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Second slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Third slide">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    <!-- </div> -->
+                </div> <!-- 1st col close--> 
+
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active img-thumbnail">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Second slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Third slide">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                </div>           
+
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active img-thumbnail">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Second slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/img/vk.jpg" alt="Third slide">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                </div>           
+
+            </div>
+        </div>
+    </section>
+
+      
+
+    <?php include "include/foot.php" ?>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
